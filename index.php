a29f64a1b068e313cf2255ceb6b5e62539ad2c33
Josh Espinoza <joshespi@gmail.com>
2023-06-24 13:05:33 -0600

    simplified

--- index.php
+++ index.php
@@ -21,37 +21,16 @@
         </nav>
     </header>
     <main>
-        <h1>Keeping Up</h1>
-        <section>
-            <?php
-            // The Query
-            $main_args = array(
-                'post_type' => 'post',
-                // 'posts_per_page' => 5
-            );
-            $main_query = new WP_Query($main_args);
-
-            // The Loop
-            if ($main_query->have_posts()) {
-                while ($main_query->have_posts()) {
-                    $main_query->the_post();
-            ?>
-                    <article>
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-
-                    </article>
-            <?php
-                }
-            } else {
-                // If no posts match the query
-                echo "No posts found.";
-            }
-
-            // Restore original post data
-            wp_reset_postdata();
-            ?>
-        </section>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                </div>
+            </article>
+        <?php endwhile; else : ?>
+            <p>No posts found.</p>
+        <?php endif; ?>
     </main>
     <?php wp_footer(); ?>
 </body>
