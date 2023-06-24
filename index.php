1928b3343113ea4e8a5211b38d74f3ed3911f920
Josh Espinoza <joshespi@gmail.com>
2023-06-24 09:40:33 -0600

    added some basic theme pulls and loop

--- index.php
+++ index.php
@@ -1,33 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
+
 <body>
+<header>
+    <h1>Espinoza Family</h1>
+    <nav></nav>
+</header>
+    <?php
+    // The Query
+    $main_args = array(
+        'post_type' => 'post',
+        // 'posts_per_page' => 5
+    );
+    $main_query = new WP_Query($main_args);
 
-<?php
+    // The Loop
+    if ($main_query->have_posts()) {
+        while ($main_query->have_posts()) {
+            $main_query->the_post();
+    ?>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="entry-content">
+                <?php the_excerpt(); ?>
+            </div>
+    <?php
+        }
+    } else {
+        // If no posts match the query
+        echo "No posts found.";
+    }
 
+    // Restore original post data
+    wp_reset_postdata();
+    ?>
 
-  // Start “The Loop”
-  if ( have_posts() ) : // Does our website have any posts to  display?
-      while (  have_posts() ) : the_post(); // If the answer is “yes”, let’s run some code.
-
-          the_title( '<h2>', '</h2>' );  // Let’s display the title of our post.
-          the_content();  // Let’s display the content of our post.
-
-      endwhile; // There  are no more posts to display, let’s shut this down.
-
-  else : // If we don’t have any posts, let’s display a custom  message below.
-
-      _e( 'Sorry, no  posts matched your criteria.', 'textdomain' );
-
-  endif; // OK, we can stop looking for posts now.
-
-  // End of “The Loop”
-
-?>
 </body>
+
 </html>
